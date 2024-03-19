9e03f3012ffe6962cbb1269236c9be66012eb24d
AmeliaPrzybyll <Amelia_przybyl@interia.pl>
2024-03-19 17:27:00 +0100

    Update 2

--- ShoppingList/obj/Debug/net8.0-windows/ShoppingList.AssemblyInfo.cs
+++ ShoppingList/obj/Debug/net8.0-windows/ShoppingList.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ShoppingList")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f57dbbf0e5019935733885f03bd3e5c92b820d63")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fcf2035c7d18725f3c4fbf7d2882c3be3bca8235")]
 [assembly: System.Reflection.AssemblyProductAttribute("ShoppingList")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ShoppingList")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
